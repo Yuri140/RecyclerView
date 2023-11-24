122e54c1486b04fab1eceef594c00141cb211fd1
Yuri140 <yurisousa.carneiro2006@gmail.com>
2023-11-23 22:13:54 -0300

    Acabou por hj

--- app/src/main/java/br/com/etecia/myapp/MainActivity.java
+++ app/src/main/java/br/com/etecia/myapp/MainActivity.java
@@ -1,6 +1,7 @@
 package br.com.etecia.myapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
@@ -14,5 +15,12 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         idRecyclerViewFilmes = findViewById(R.id.idRecyclerViewFilmes);
+
+        idRecyclerViewFilmes.setLayoutManager(new GridLayoutManager(getApplicationContext(),2));
+
+        idRecyclerViewFilmes.setHasFixedSize(true);
+
+        //idRecyclerViewFilmes.setAdapter(adapter);
+        //https://www.amazon.com.br/Prime-Video-Filmes/s?rh=n%3A18730410011%2Cp_n_entity_type%3A19218549011&page=3
     }
 }
